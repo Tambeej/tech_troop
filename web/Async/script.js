39cb9a278d7e769d23d80bbfc72afa764b1ca984
Tambeej <bejella@gmail.com>
2025-07-29 13:53:02 +0300

    solved ex 2

--- web/Async/script.js
+++ web/Async/script.js
@@ -1,3 +1,5 @@
+const { post } = require("jquery");
+
 //Ex 1.
 async function getUserById(userId) {
   try {
@@ -16,3 +18,37 @@ async function getUserById(userId) {
     return null;
   }
 }
+
+//Ex 2.
+async function getUserWithPosts(userId) {
+  try {
+    const userResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${userId}`
+    );
+    if (!userResponse.ok) {
+      throw new Error("User not found");
+    }
+
+    const user = await userResponse.json();
+    try {
+      const postResponse = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+      );
+      if (!postResponse.ok) {
+        throw new Error("Post not found");
+      }
+
+      const posts = await postResponse.json();
+      let usersPost = `${user.name} ${posts}`;
+      console.log(usersPost);
+    } catch (error) {
+      console.error("Error fetching post:", error.message);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error.message);
+    return null;
+  }
+}
+
+console.log(getUserWithPosts(1));
