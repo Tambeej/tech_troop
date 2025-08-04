8b7859dc9513073a161513d3d3cf90d6533d2796
Tambeej <bejella@gmail.com>
2025-08-04 15:01:13 +0300

    ex 2 done

--- web/React/react-demo/src/App.js
+++ web/React/react-demo/src/App.js
@@ -8,6 +8,16 @@ function App() {
     );
   };
 
+  const getClassName = (temperature) => {
+    if (temperature < 15) {
+      return "freezing";
+    } else if (temperature <= 30) {
+      return "fair";
+    } else {
+      return "hell-scape";
+    }
+  };
+
   let companies = [
     { name: "Tesla", revenue: 140 },
     { name: "Microsoft", revenue: 300 },
@@ -15,10 +25,17 @@ function App() {
   ];
   return (
     //ex1.
+    // <div className="ex-space">
+    //   <h4 className="ex-title">Exercise 1</h4>
+    //   <div className="exercise" id="ex-1">
+    //     {companies.map((c) => showCompany(c.name, c.revenue))}
+    //   </div>
+    // </div>
+    //ex2.
     <div className="ex-space">
-      <h4 className="ex-title">Exercise 1</h4>
-      <div className="exercise" id="ex-1">
-        {companies.map((c) => showCompany(c.name, c.revenue))}
+      <h4 className="ex-title">Exercise 2</h4>
+      <div className="exercise" id="ex-2">
+        <div id="weatherBox" className={getClassName(40)}></div>
       </div>
     </div>
   );
