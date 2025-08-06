890e9c4489e0f7a87a577ae9a4aa864ed72ba61f
Tambeej <bejella@gmail.com>
2025-08-06 15:42:12 +0300

    Ex 2 Solved

--- web/React/input-demo/src/App.jsx
+++ web/React/input-demo/src/App.jsx
@@ -4,13 +4,16 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Exercise1 from "./components/Exersice1";
 
+import Exercise2 from "./components/Exersice2";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <Exercise1 />
+        {/* <Exercise1 /> */}
+        <Exercise2 />
       </div>
     </>
   );
---
+++ web/React/input-demo/src/components/Exersice2.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Exercise2 = () => {
+  const [name, setName] = useState("");
+  const [fruit, setFruit] = useState("");
+  const handleFruitChange = (e) => {
+    const selectedFruit = e.target.value;
+    setFruit(selectedFruit);
+
+    console.log(`${name} selected ${selectedFruit}`);
+  };
+
+  return (
+    <div>
+      <input
+        id="name-input"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <select id="select-input" onChange={handleFruitChange} value={fruit}>
+        <option value="grapes">Grapes</option>
+        <option value="date">Date</option>
+        <option value="mango">Mango</option>
+        <option value="banana">Banana</option>
+      </select>
+    </div>
+  );
+};
+export default Exercise2;
