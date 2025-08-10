b1ad6313b513b27ccc21a5fabd0da9febf1eab0e
Tambeej <bejella@gmail.com>
2025-08-10 22:56:49 +0300

    ex2 Done

---
+++ web/React/data-flow-demo/src/components/Exercise2.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import List from "./List";
+import Conversation from "./Conversation";
+
+export default function Exercise2() {
+  const [displayConversation, setDisplayConversation] = useState("");
+  const [conversations, setConversation] = useState([
+    {
+      with: "Laura",
+      convo: [
+        { text: "Hi", sender: "self" },
+        { text: "You there?", sender: "self" },
+        { text: "Yeah, hi, what's up?", sender: "other" },
+      ],
+    },
+    {
+      with: "Dad",
+      convo: [
+        { text: "Have you finished your school work yet?", sender: "other" },
+        { text: "Yes.", sender: "self" },
+        { text: "What do you mean, yes?", sender: "other" },
+        { text: "??", sender: "self" },
+      ],
+    },
+    {
+      with: "Shoobert",
+      convo: [
+        { text: "Shoobert!!!", sender: "self" },
+        { text: "Dude!!!!!!!!", sender: "other" },
+        { text: "Shooooooooo BERT!", sender: "self" },
+        { text: "You're my best friend", sender: "other" },
+        { text: "No, *you're* my best friend", sender: "self" },
+      ],
+    },
+  ]);
+
+  return (
+    <div>{displayConversation === null ? <List /> : <Conversation />}</div>
+  );
+}
---
+++ web/React/data-flow-demo/src/components/List.jsx
@@ -0,0 +1,3 @@
+export default function List() {
+  return <div>List of contacts here</div>;
+}
