f60ef1dc95eb0da4beedd06ad9d7cf428f768fbf
Tambeej <bejella@gmail.com>
2025-08-25 23:25:32 +0300

    fixed

--- backend/server-intro/server.js
+++ backend/server-intro/server.js
@@ -3,23 +3,19 @@ const http = require("http");
 const server = http.createServer(function (request, response) {
   console.log(`method: ${request.method} url: ${request.url}`);
   if (request.method === "GET" && request.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to my server!");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Welcome to my server!");
   } else if (request.method === "GET" && request.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("This is the about page");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("This is the about page");
   } else if (request.method === "GET" && request.url === "/contact") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Contact me at: tamarboyanjo@email.com");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Contact me at: tamarboyanjo@email.com");
   } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.end("404 Not Found");
   }
-  response.end();
 });
-
-// GET /contact - Return your contact information
-// Any other route - Return "404 - Page not found" with status code 404
 
 const port = 3000;
 server.listen(port, function () {
