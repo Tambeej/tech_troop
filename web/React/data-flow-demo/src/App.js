e38cba1f2f53423cf2692fe86f12805550e0c4ab
Tambeej <bejella@gmail.com>
2025-08-25 23:42:28 +0300

    Merge branch 'main' of https://github.com/Tambeej/tech_troop for pushing new exs

---
+++ web/React/data-flow-demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Exercise2 from "./components/Exercise2";
+
+function App() {
+  return (
+    <div className="App">
+      <Exercise2 />
+    </div>
+  );
+}
+
+export default App;
