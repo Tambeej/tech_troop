64467fee28bcce3e5901d97a7bea9494c7e07dc9
Tambeej <bejella@gmail.com>
2025-08-26 22:58:48 +0300

    ex5 done

--- backend/furniture-store/dist/main.js
+++ backend/furniture-store/dist/main.js
@@ -1,8 +1,26 @@
 const fetchItemData = function () {
   let input = $("#item-input").val();
-  console.log(input);
 
   $.get(`priceCheck/${input}`, function (itemData) {
     $("body").append(`<div>${itemData.price}`);
+  });
+};
+
+const updateInventory = function () {
+  let input = $("#buy-input").val();
+
+  $.get(`buy/${input}`, function (itemData) {
+    if (itemData.message) {
+      // Handle out of stock or item not found
+      $("body").append(
+        `<div style="color:red;">${itemData.message}${
+          itemData.item ? ` - ${itemData.item.name}` : ""
+        }</div>`
+      );
+    } else {
+      $("body").append(
+        `<div>Congratulations, you've just bought ${itemData.name} for ${itemData.price}. There are ${itemData.inventory} left now in the store.`
+      );
+    }
   });
 };
