00f63b5d8c7956264ce5c1b99731260564b23203
Tambeej <bejella@gmail.com>
2025-08-26 22:24:48 +0300

    ex 1 for real

--- backend/furniture-store/server.js
+++ backend/furniture-store/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-app.use(express.static(path.join(__dirname, "dist")));
-app.use(express.static(path.join(__dirname, "..", "node_modules")));
+// app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "..", "node_modules")));
+
+const store = [
+  { name: "table", inventory: 3, price: 800 },
+  { name: "chair", inventory: 16, price: 120 },
+  { name: "couch", inventory: 1, price: 1200 },
+  { name: "picture frame", inventory: 31, price: 70 },
+];
 
 app.get("/", function (request, response) {
+  console.log("here");
   response.send("Server is up and running smoothly");
 });
 
