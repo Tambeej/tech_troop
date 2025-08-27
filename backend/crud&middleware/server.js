d8d0cc58d763a8ec632fb1ceb8ff7722dfcad0c7
Tambeej <bejella@gmail.com>
2025-08-28 00:03:13 +0300

    wx1

---
+++ backend/crud&middleware/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${req.method} ${req.url}`);
+  next(); 
+});
+
+let counter = 0;
+app.use((req, res, next) => {
+  counter++;
+  req.requestCount = counter; 
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to the Home Page!",
+    requestCount: req.requestCount,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send({
+    message: "This is the About Page!",
+    requestCount: req.requestCount,
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
